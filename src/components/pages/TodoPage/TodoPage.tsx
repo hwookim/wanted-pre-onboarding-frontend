6118275fd32e3a9c7ce50591a848a7b6c41b4ec8
hwookim <gusdn9754@gmail.com>
2023-04-08 23:58:50 +0900

    feat: Todo 추가 및 토글 기능 구현

--- src/components/pages/TodoPage/TodoPage.tsx
+++ src/components/pages/TodoPage/TodoPage.tsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { ChangeEvent, useRef, useState } from 'react';
 
 const TodoPage: React.FC = () => {
-  return <>todo</>;
+  const [newTodoValue, setNewTodoValue] = useState<string>('');
+  const [todos, setTodos] = useState<
+    { id: number; todo: string; isCompleted: boolean }[]
+  >([]);
+  const id = useRef(todos[todos.length - 1]?.id || 0);
+
+  const handleNewTodo = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setNewTodoValue(value);
+  };
+
+  const handleAddTodo = () => {
+    const newTodo = {
+      id: id.current++,
+      todo: newTodoValue,
+      isCompleted: false,
+    };
+    setTodos((prev) => [...prev, newTodo]);
+    setNewTodoValue('');
+  };
+
+  const handleToggleTodo = (id: number) => () => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id !== id ? todo : { ...todo, isCompleted: !todo.isCompleted },
+      ),
+    );
+  };
+
+  return (
+    <>
+      <input
+        data-testid="new-todo-input"
+        value={newTodoValue}
+        onChange={handleNewTodo}
+      />
+      <button data-testid="new-todo-add-button" onClick={handleAddTodo}>
+        추가
+      </button>
+      <ul>
+        {todos.map(({ id, todo, isCompleted }, index) => (
+          <li key={index}>
+            <label>
+              <input
+                type="checkbox"
+                checked={isCompleted}
+                onChange={handleToggleTodo(id)}
+              />
+              <span>{todo}</span>
+            </label>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default TodoPage;
